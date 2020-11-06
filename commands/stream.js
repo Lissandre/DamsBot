5c8f84044d2398a9d5fd86aa9130f6988b0e37e7
Lissandre <lissandre.pasdeloup@hetic.net>
2020-11-06 22:51:52 +0100

    wtah2gether impl

---
+++ commands/stream.js
@@ -0,0 +1,23 @@
+const axios = require('axios')
+
+module.exports = {
+  name: 'stream',
+  description: 'Dams stream',
+  async execute(msg, args) {
+    const data = JSON.stringify({ 
+      "w2g_api_key" : process.env.WATCH_API,
+      "share" : args,
+      "bg_color" : "#545454",
+      "bg_opacity" : "50"
+    })
+    axios
+      .post('https://w2g.tv/rooms/create.json', data)
+      .then(res => {
+        msg.reply(`Let's go: https://w2g.tv/rooms/${res}`)
+      })
+      .catch(error => {
+        msg.channel.send("J'ai pas réussi à créer la room :(")
+        console.error(error)
+      })
+  },
+}
