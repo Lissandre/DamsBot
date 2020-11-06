835b47e13830ac0fa90ab90965ac4c2cf1435923
Lissandre <lissandre.pasdeloup@hetic.net>
2020-11-06 20:56:05 +0100

    update path sound

--- commands/speak.js
+++ commands/speak.js
@@ -4,7 +4,12 @@ module.exports = {
   async execute(msg, args) {
     if (msg.member.voice.channel) {
       const connection = await msg.member.voice.channel.join()
-      const dispatcher = connection.play('../sounds/death.mp3')
+      const dispatcher = connection.play('./sounds/death.mp3', { volume: 1 })
+      dispatcher.on('finish', () => {
+        setTimeout(()=>{
+          connection.disconnect()
+        }, 500)
+      })
     }
     else {
       msg.reply('Tu dois être dans un channel audio bébou...')
