1138eb68d8111d1dd9d31fdd6b6f2de443f734c1
Lissandre <lissandre.pasdeloup@hetic.net>
2020-11-06 21:09:42 +0100

    fart

---
+++ commands/fart.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: 'fart',
+  description: 'Dams prout',
+  async execute(msg, args) {
+    if (msg.member.voice.channel) {
+      const connection = await msg.member.voice.channel.join()
+      const dispatcher = connection.play('./sounds/fart.mp3', { volume: 1000 })
+      dispatcher.on('finish', () => {
+        setTimeout(()=>{
+          connection.disconnect()
+        }, 500)
+      })
+    }
+    else {
+      msg.reply('Tu dois être dans un channel audio bébou...')
+    }
+  },
+}
