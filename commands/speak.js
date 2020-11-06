bbea6e277a9367eaf8386a646d0ae8f7892b194a
Lissandre <lissandre.pasdeloup@hetic.net>
2020-11-06 20:29:00 +0100

    sounds

--- commands/speak.js
+++ commands/speak.js
@@ -4,7 +4,7 @@ module.exports = {
   async execute(msg, args) {
     if (msg.member.voice.channel) {
       const connection = await msg.member.voice.channel.join()
-      const dispatcher = connection.play(require('../sounds/death.mp3'))
+      const dispatcher = connection.play('../sounds/death.mp3')
     }
     else {
       msg.reply('Tu dois être dans un channel audio bébou...')
