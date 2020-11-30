fde02cfb393add631751b2d0584dcd3f75ef94ae
Lissinge <lissandre.pasdeloup@hetic.net>
2020-11-30 22:50:02 +0100

    add snap soft

--- commands/snap.js
+++ commands/snap.js
@@ -24,7 +24,7 @@ module.exports = {
           msg.guild.member(user.id).voice.setChannel(null)
         }
         else {
-          msg.guild.member(user.id).voice.mute()
+          msg.guild.member(user.id).voice.setMute(true)
         }
       })
     }
